81c50544b8b2540dfe6e8966d2fef922e4762385
wesamAlsaleh <wesammuneer7@gmail.com>
2026-03-24 20:46:57 +0300

    Initial commit

---
+++ src/main/java/com/avocadogroup/concurrentprocessor/ConcurrentProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.avocadogroup.concurrentprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avocadogroup/concurrentprocessor/ConcurrentProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocadogroup.concurrentprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
